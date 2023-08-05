16f2a592270bc7170d4bf48cc38c3454d8409737
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-06 02:13:26 +0530

    feat: consume resources endpoint in frontend

--- client/src/pages/RoadmapDetail.jsx
+++ client/src/pages/RoadmapDetail.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
 export default function RoadmapDetail() {
   const location = useLocation();
-  const roadmapSteps = location.state.roadmap;
-  console.log(roadmapSteps);
+  const roleInfo = location.state.roleInfo;
+  const [resources, setResources] = useState();
+
+  useEffect(() => {
+    async function getResources() {
+      console.log("fetching data")
+      const res = await axios.get(`http://127.0.0.1:8000/api/resources?role=${roleInfo.name}`)
+      setResources(res.data);
+    }
+    getResources();
+  }, [roleInfo])
+
+  console.log(resources);
+
   return (
     <div>Roadmap details goes here!</div>
   );
