db13d74d13f435b0174a70257e1df431dfc8c351
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-05 12:30:36 +0530

    fix: navbar links

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function LoginButton() {
   const {loginWithRedirect} = useAuth0();
   return (
-    <button onClick={() => loginWithRedirect()} className="loginBtn">
+    <button onClick={() => loginWithRedirect()} className="text-xl hover:text-green-500 duration-500">
       Log In
     </button>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,19 +13,19 @@ export default function Navbar() {
 
         <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[400px] transition-all ease-in duration-500">
           <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-green-500 duration-500"><NavLink to="/">Home</NavLink></a>
+            <NavLink className="text-xl hover:text-green-500 duration-500" to="/">Home</NavLink>
           </li>
           <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-green-500 duration-500"><LoginButton /></a>
+            <LoginButton />
           </li>
           <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-green-500 duration-500"><LogoutButton /></a>
+            <LogoutButton />
           </li>
           <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-green-500 duration-500"><NavLink to="/profile">Profile</NavLink></a>
+            <NavLink className="text-xl hover:text-green-500 duration-500" to="/profile">Profile</NavLink>
           </li>
           <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-green-500 duration-500"><NavLink to="/roadmap">Roadmap</NavLink></a>
+            <NavLink className="text-xl hover:text-green-500 duration-500" to="/roadmap">Roadmap</NavLink>
           </li>
 
 
