0f8d61c1bd8a884aa6cbdbe3fa2972c2b5dbd7a5
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-07 01:49:31 +0530

    animate loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,11 @@
+import loaderImage from "../assets/images/loader.png"
+import "../index.css"
+
 export default function Loader() {
   return (
-    <div>
-      Loading...
+    <div className="h-[63vh] flex flex-col justify-center items-center">
+      <img src={loaderImage} alt="loading" className="loader"/>
+      <h3 className="font-inter text-[2rem]">Please Wait ... We are processing your data...</h3>
     </div>
   );
 }
