24e0f482db6c8ade7d6a115ddb60f12fbf5d22b9
itshypon <himanshupoonia336@gmail.com>
2023-08-06 22:40:03 +0530

    all pages ready

--- client/src/pages/BookMarks/RoadMapBM.jsx
+++ client/src/pages/BookMarks/RoadMapBM.jsx
@@ -1,7 +1,58 @@
+import RoadmapStep from "../../components/RoadmapStep";
+
 export default function RoadMapBM() {
   return (
-    <div>
-      Bookmark kiye hue roadmaps
+    <div className="mt-[4.9375rem] ml-[13.5rem] flex py-[3.25rem] px-[4.25rem] gap-32">
+      <div className="col-1 flex flex-col gap-4">
+        <div className="title text-3xl font-bold mb-2px">
+          SOFTWARE ENGINEER - ROADMAP
+        </div>
+        <RoadmapStep 
+        number = "1"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "2"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "3"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "4"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "5"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+      </div>
+      <div className="col-2 flex flex-col gap-4">
+      <div className="title text-3xl font-bold mb-2px">
+          SOFTWARE ENGINEER - ROADMAP
+        </div>
+        <RoadmapStep 
+        number = "1"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "2"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "3"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "4"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+        <RoadmapStep 
+        number = "5"
+        content = "Master programming languages like Python, Java, or C++."
+        />
+      </div>
     </div>
   );
 }
--- client/src/pages/BookMarks/YoutubeBM.jsx
+++ client/src/pages/BookMarks/YoutubeBM.jsx
@@ -1,14 +1,39 @@
-import RoadmapStep from "../../components/RoadmapStep";
+import Card from "../../components/Card";
 
 export default function YoutubeBM() {
   return (
-    <div className="mt-[4.9375rem] flex ">
-      <div className="col-1">
-
-      </div>
-      <div className="col-2">
-
-      </div>
+    <div className="mt-[3.125rem] ml-[13.5rem] mr-[11.25rem] flex flex-col gap-4 py-[3.875rem] px-[5.625rem]">
+      <Card 
+        title = "Complete Web Developer Bootcamp"
+        content = "Web Development Tutorials"
+        type = "yt"
+        // link = {}
+      />
+      <Card 
+        title = "Complete Web Developer Bootcamp"
+        content = "Web Development Tutorials"
+        type = "yt"
+        // link = {}
+      />
+      <Card 
+        title = "Complete Web Developer Bootcamp"
+        content = "Web Development Tutorials"
+        type = "yt"
+        // link = {}
+      />
+      <Card 
+        title = "Complete Web Developer Bootcamp"
+        content = "Web Development Tutorials"
+        type = "yt"
+        // link = {}
+      />
+      <Card 
+        title = "Complete Web Developer Bootcamp"
+        content = "Web Development Tutorials"
+        type = "yt"
+        // link = {}
+      />
+      
     </div>
   );
 }
