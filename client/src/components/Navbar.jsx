5aaefd1ec8b3e745ab84ccfbfdeeeafc982fe309
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-08 03:15:02 +0530

    change navbar font to inter

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
         <div className="cursor-pointer flex justify-between items-center">
           <img src={Logo} alt="" />
         </div>
-        <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[400px] transition-all ease-in duration-500">
+        <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[400px] transition-all ease-in duration-500 font-inter">
           <li className="mx-4 my-6 md:my-0">
             <NavLink className={({ isActive }) => (`text-xl hover:text-green-500 duration-500 ${isActive ? "text-green-500" : ""}`)} to="/">Home</NavLink>
           </li>
