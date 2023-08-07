48bc3b0d4f7efb969f391d51df78097245ebb0a3
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-08 03:14:32 +0530

    change navbar BG

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,11 +16,10 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+      <div className="p-5 bg-[#CFFFD9] shadow md:flex md:items-center md:justify-between">
         <div className="cursor-pointer flex justify-between items-center">
           <img src={Logo} alt="" />
         </div>
-
         <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[400px] transition-all ease-in duration-500">
           <li className="mx-4 my-6 md:my-0">
             <NavLink className={({ isActive }) => (`text-xl hover:text-green-500 duration-500 ${isActive ? "text-green-500" : ""}`)} to="/">Home</NavLink>
