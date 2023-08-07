33a637403894e247550a46f99405d82f716bbc37
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-08-08 03:23:24 +0530

    navbar refactor and spacing adjustment

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,9 +16,9 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className="p-5 bg-[#CFFFD9] shadow md:flex md:items-center md:justify-between">
-        <div className="cursor-pointer flex justify-between items-center">
-          <img src={Logo} alt="" />
+      <div className="p-5 bg-[#CFFFD9] px-[3rem] shadow md:flex md:items-center md:justify-between">
+        <div className="cursor-pointer ">
+          <img src={Logo} alt="logo" />
         </div>
         <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[400px] transition-all ease-in duration-500 font-inter">
           <li className="mx-4 my-6 md:my-0">
