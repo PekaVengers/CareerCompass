fd988f55880dd99e13fcd8bfbc08a546bebefe11
itshypon <himanshupoonia336@gmail.com>
2023-08-08 07:39:03 +0530

    Adds model to book session

--- client/src/components/MentorCard.jsx
+++ client/src/components/MentorCard.jsx
@@ -1,5 +1,18 @@
 import mentorImg from "../assets/mentorimg.png"
+import SmallBox from "./SmallBox"
+import { useState } from "react"
+
 export default function MentorCard(props) {
+    const [isBoxOpen, setIsBoxOpen] = useState(false);
+
+    function handleButtonClick(){
+        setIsBoxOpen(true);
+    }
+
+    function handleButtonClose(){
+        setIsBoxOpen(false);
+    }
+
     return(
     <div className="container flex max-h-[255px] mb-[27px] rounded-3xl">
         <img className="rounded-l h-[100%] w-[100%] object-cover" src={mentorImg} alt="" />
@@ -16,7 +29,8 @@ export default function MentorCard(props) {
                 <div className="link text-sm underline">{props.mail}</div>
             </div>
             <div className="button">
-                <button className="bg-green-600 px-4 py-2 text-white rounded-2xl font-semibold">Book Session</button>
+                <button className="bg-green-600 px-4 py-2 text-white rounded-2xl font-semibold" onClick={handleButtonClick}>Book Session</button>
+                {isBoxOpen && <SmallBox onClose = {handleButtonClose}/>}
             </div>
         </div>
     </div>
